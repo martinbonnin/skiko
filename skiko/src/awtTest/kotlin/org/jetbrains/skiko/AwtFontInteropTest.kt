cedbbf9aee3dce7e2b5e72e10dd015cbca678d5f
dima.avdeev <dima.avdeev@jetbrains.com>
2022-12-07 14:01:35 +0400

    run tests in parallel

--- skiko/src/awtTest/kotlin/org/jetbrains/skiko/AwtFontInteropTest.kt
+++ skiko/src/awtTest/kotlin/org/jetbrains/skiko/AwtFontInteropTest.kt
@@ -15,5 +15,97 @@ class AwtFontInteropTest {
         Assume.assumeTrue(hostOs != OS.Linux)
     }
 
+    @OptIn(DelicateSkikoApi::class)
+    @Test
+    fun canFindAvailableFont() = fontManager.whenAllFontsCachedBlocking {
+        assumeOk()
+        val font = Font("Verdana", Font.BOLD, 12)
+        val path = fontManager.findAvailableFontFile(font)
+        assertTrue("Font must be found", path != null)
+        path!!
+        assertTrue("Font must be file", path.exists() && path.isFile)
+    }
 
+    @Test
+    fun canFindFont() {
+        runTest {
+            assumeOk()
+            val font = Font("Verdana", Font.BOLD, 12)
+            val path = fontManager.findFontFile(font)
+            assertTrue("Font must be found", path != null)
+            path!!
+            assertTrue("Font must be file", path.exists() && path.isFile)
+        }
+    }
+
+    @Test
+    fun canFindFamily() {
+        runTest {
+            assumeOk()
+            val path = fontManager.findFontFamilyFile("Verdana")
+            assertTrue("Font must be found", path != null)
+            path!!
+            assertTrue("Font must be file", path.exists() && path.isFile)
+        }
+    }
+
+    @OptIn(DelicateSkikoApi::class)
+    @Test
+    fun nonExistentFont() = fontManager.whenAllFontsCachedBlocking {
+        assumeOk()
+        val font = Font("XXXYYY745", Font.BOLD, 12)
+        val path = fontManager.findAvailableFontFile(font)
+        assertTrue("Font must not be found", path == null)
+    }
+
+    @Test
+    fun makeSkikoTypeface() {
+        runTest {
+            assumeOk()
+            Assume.assumeFalse(GraphicsEnvironment.isHeadless())
+            val font = Font("Verdana", Font.BOLD, 12)
+            val skikoTypeface = font.toSkikoTypeface()
+            assertTrue("Skiko typeface must work", skikoTypeface != null)
+            skikoTypeface!!
+            assertTrue("Skiko typeface name is incorrect: ${skikoTypeface.familyName}", skikoTypeface.familyName == "Verdana")
+        }
+    }
+
+    @Test
+    fun listAllFonts() {
+        runTest {
+            assumeOk()
+            val fontFiles = fontManager.listFontFiles()
+            assertTrue("There must be fonts", fontFiles.isNotEmpty())
+        }
+    }
+
+    @Test
+    fun addCustomPath() {
+        runTest {
+            assumeOk()
+            val resDir = System.getProperty("skiko.test.font.dir")!!
+            fontManager.addCustomPath(resDir)
+            fontManager.invalidate()
+            val path = fontManager.findFontFamilyFile("JetBrains Mono")
+            assertTrue("Custom font must be found", path != null)
+            path!!
+            assertTrue("Font must be file", path.exists() && path.isFile)
+        }
+    }
+
+    // This test is disabled due to convoluted setup of tests.
+    // @Test
+    fun addCustomResource() {
+        runTest {
+            assumeOk()
+            val fontManager = AwtFontManager()
+            assertTrue("Custom resource must be found",
+                fontManager.addResourceFont("/fonts/JetBrainsMono-Bold.ttf", Library.javaClass.classLoader))
+            val path = fontManager.findFontFamilyFile("JetBrains Mono")
+            assertTrue("Custom font must be found", path != null)
+            path!!
+            assertTrue("Font must be file", path.exists() && path.isFile)
+        }
+    }
 }
