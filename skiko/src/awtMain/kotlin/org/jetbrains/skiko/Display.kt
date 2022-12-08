dbe62a3f73695f62048ae706d0913aaef9c598a2
dima.avdeev <dima.avdeev@jetbrains.com>
2022-12-08 17:51:14 +0400

    hardcode getDisplayRefreshRate

--- skiko/src/awtMain/kotlin/org/jetbrains/skiko/Display.kt
+++ skiko/src/awtMain/kotlin/org/jetbrains/skiko/Display.kt
@@ -8,9 +8,10 @@ internal const val MinMainstreamMonitorRefreshRate = 60.0
 internal fun HardwareLayer.getDisplayRefreshRate(): Double {
     // We use different method for Linux, because it.displayMode.refreshRate returns always a wrong value: 50 (probably because of the using the old xrandr API)
     return if (hostOs == OS.Linux) {
-        lockLinuxDrawingSurface {
-            getLinuxDisplayRefreshRate(it.display, it.window)
-        }
+        return 60.0
+//        lockLinuxDrawingSurface {
+//            getLinuxDisplayRefreshRate(it.display, it.window)
+//        }
     } else {
         graphicsConfiguration
             .device
